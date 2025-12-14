07de9f419785ebf5335cbd5c71e9594e76b4fa2d
JianZcar <esteban.jianzcar@outlook.com>
2025-12-14 21:53:43 +0800

    install bootc

--- build_files/00-base.sh
+++ build_files/00-base.sh
@@ -21,26 +21,40 @@ EOF
 pacman-key --init
 pacman -Syu --noconfirm
 
-# Import the repository key
 pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
-# Sign the repository key
 pacman-key --lsign-key F3B607488DB35A47
+
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+
+
 
 pacman -U --noconfirm \
   'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-keyring-20240331-1-any.pkg.tar.zst' \
   'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-mirrorlist-22-1-any.pkg.tar.zst' \
-  'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-v3-mirrorlist-22-1-any.pkg.tar.zst'
+  'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-v3-mirrorlist-22-1-any.pkg.tar.zst' \
+  'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' \
+  'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 cat <<EOF > /tmp/cachyos-repos
 [cachyos-v3]
 Include = /etc/pacman.d/cachyos-v3-mirrorlist
+
 [cachyos-core-v3]
 Include = /etc/pacman.d/cachyos-v3-mirrorlist
+
 [cachyos-extra-v3]
 Include = /etc/pacman.d/cachyos-v3-mirrorlist
+
 [cachyos]
 Include = /etc/pacman.d/cachyos-mirrorlist
 
+EOF
+
+cat <<EOF >> /etc/pacman.conf
+
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
 EOF
 
 cat /etc/pacman.conf
@@ -70,6 +84,8 @@ packages=(
   dbus-glib
   glib2
   shadow
+
+  chaotic-aur/bootc
 )
 
 pacman-key --populate archlinux cachyos
